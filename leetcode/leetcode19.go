2b5b4d8ccc0a63334a36e6ba30c1f2bb2491eaee
Ajaykumar <ajaykumar@futurewise.co.in>
2023-10-04 21:46:27 +0530

    new problem solution added

---
+++ leetcode/leetcode19.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Next: head, Val: 0}
+	left := dummy
+	right := head
+	for i := 0; i < n; i++ {
+		right = right.Next
+	}
+	for right != nil {
+		left = left.Next
+		right = right.Next
+	}
+	left.Next = left.Next.Next
+	return dummy.Next
+}
